559ff4c4d11b72bfd75c01e4d73e4f08fe350da9
JarWarren <wrrn24@gmail.com>
2024-08-07 12:28:36 -0600

    generate fsm objects

--- packages/twenty-server/src/funnelmink/funnelmink-objects-prefill-data.ts
+++ packages/twenty-server/src/funnelmink/funnelmink-objects-prefill-data.ts
@@ -9,6 +9,9 @@ import { ObjectMetadataService } from 'src/engine/metadata-modules/object-metada
 import {
   STANDARD_OBJECT_IDS,
 } from 'src/engine/workspace-manager/workspace-sync-metadata/constants/standard-object-ids';
+import { FUNNELMINK_ICONS } from 'src/funnelmink/funnelmink-constants';
+import { CreateObjectInput } from 'src/engine/metadata-modules/object-metadata/dtos/create-object.input';
+import { object } from 'zod';
 
 export const prefillWorkspaceWithFunnelminkFSMObjects = async (
   dataSourceMetadata: DataSourceEntity,
@@ -25,13 +28,18 @@ export const prefillWorkspaceWithFunnelminkFSMObjects = async (
     throw new Error('Could not connect to workspace data source');
   }
 
-  // disable standard objects
-  // await disableStandardObjects(objectMetadataService);
+  // loop through create funnelmink objects (function)
+  await createFunnelminkObjects(workspaceId, objectMetadataService);
 
-  // create funnelmink objects
+  // loop through and add create fields (function)
+  // - go item by item
+  // - check my prior branch for which fields go on what
+  // - stickyNote, etc
 
+  // loop through and create relationships (function)
+  // - again, check my prior branch
 
-  // populate funnelmink objects with prefill data
+  // lastly, prefill data (function)
 };
 
 // const funnelminkObjectsPrefillData = async (
@@ -85,3 +93,142 @@ const disableStandardObjects = async (
     );
   }));
 };
+
+const createFunnelminkObjects = async (
+  workspaceId: string,
+  objectMetadataService: ObjectMetadataService,
+) => {
+
+  for (const object of objects) {
+    const input: CreateObjectInput = {
+      dataSourceId: '',
+      workspaceId: workspaceId,
+      nameSingular: object.nameSingular,
+      namePlural: object.namePlural,
+      labelSingular: object.labelSingular,
+      labelPlural: object.labelPlural,
+      description: object.description,
+      icon: object.icon,
+      isRemote: object.isRemote,
+    };
+    const objectMetadata = await objectMetadataService.createOne(input);
+
+    // create fields
+    // - go item by item
+    // - check my prior branch for which fields go on what
+    // - stickyNote, etc
+  }
+
+// - figure out where new objects are normally made (SettingsNewObject.tsx)
+  //    - (if I can't find a resolver, because it might not exist, then I'll have to create the objects manually)
+  //    - (this could be done by looking up the `companyPrefillData`, `personPrefillData`, and `viewPrefillData` implementations)
+  // - save sends the following data
+  /*
+  export const settingsDataModelObjectAboutFormSchema =
+    objectMetadataItemSchema.pick({
+    description: true,
+    icon: true,
+    labelPlural: true,
+    labelSingular: true,
+  });
+  * */
+  // - find the resolver
+  // - manually send my data to that same resolver
+};
+
+/* OBJECTS
+*     const objectMetadata = await this.objectMetadataService.createOne({
+      nameSingular: camelCase(localTableNameSingular),
+      namePlural: camelCase(localTableNamePlural),
+      labelSingular: camelToTitleCase(camelCase(localTableBaseName)),
+      labelPlural: camelToTitleCase(plural(camelCase(localTableBaseName))),
+      description: 'Remote table',
+      dataSourceId: dataSourceMetadataId,
+      workspaceId: workspaceId,
+      icon: 'IconPlug',
+      isRemote: true,
+      primaryKeyColumnType: distantTableIdColumn.udtName,
+      primaryKeyFieldMetadataSettings: mapUdtNameToFieldSettings(
+        distantTableIdColumn.udtName,
+      ),
+    } satisfies CreateObjectInput);
+    *
+    *
+    * FIELDS
+    *   private async createFieldMetadataForForeignTableColumn(
+    workspaceId: string,
+    columnName: string,
+    columnType: string,
+    objectMetadataId: string,
+  ): Promise<FieldMetadataEntity<'default'>> {
+    return this.fieldMetadataService.createOne({
+      name: columnName,
+      label: camelToTitleCase(columnName),
+      description: 'Field of remote',
+      type: mapUdtNameToFieldType(columnType),
+      workspaceId: workspaceId,
+      objectMetadataId: objectMetadataId,
+      isRemoteCreation: true,
+      isNullable: true,
+      icon: 'IconPlug',
+      settings: mapUdtNameToFieldSettings(columnType),
+    } satisfies CreateFieldInput);
+  }
+* */
+
+const objects = [
+  {
+    nameSingular: 'workOrder',
+    namePlural: 'workOrders',
+    labelSingular: 'Work Order',
+    labelPlural: 'Work Orders',
+    description: 'A Work Order',
+    isRemote: false,
+    icon: FUNNELMINK_ICONS.workorder,
+  },
+  {
+    nameSingular: 'service',
+    namePlural: 'services',
+    labelSingular: 'Service',
+    labelPlural: 'Services',
+    description: 'A Service',
+    isRemote: false,
+    icon: FUNNELMINK_ICONS.service,
+  },
+  {
+    nameSingular: 'crew',
+    namePlural: 'crews',
+    labelSingular: 'Crew',
+    labelPlural: 'Crews',
+    description: 'A Crew',
+    isRemote: false,
+    icon: FUNNELMINK_ICONS.crew,
+  },
+  {
+    nameSingular: 'equipment',
+    namePlural: 'equipments',
+    labelSingular: 'Equipment',
+    labelPlural: 'Equipment',
+    description: 'Equipment',
+    isRemote: false,
+    icon: FUNNELMINK_ICONS.equipment,
+  },
+  {
+    nameSingular: 'material',
+    namePlural: 'materials',
+    labelSingular: 'Material',
+    labelPlural: 'Materials',
+    description: 'Material',
+    isRemote: false,
+    icon: FUNNELMINK_ICONS.material,
+  },
+  {
+    nameSingular: 'job',
+    namePlural: 'jobs',
+    labelSingular: 'Job',
+    labelPlural: 'Jobs',
+    description: 'Job',
+    isRemote: false,
+    icon: FUNNELMINK_ICONS.job,
+  },
+];
