53c85ace2d5a856db5517d5dac809edbd49f5f2a
JarWarren <wrrn24@gmail.com>
2024-08-08 07:55:11 -0600

    alphabetize nav drawer

--- packages/twenty-front/src/modules/object-metadata/components/ObjectMetadataNavItems.tsx
+++ packages/twenty-front/src/modules/object-metadata/components/ObjectMetadataNavItems.tsx
@@ -2,14 +2,18 @@ import { useLocation } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { useIcons } from 'twenty-ui';
 
-import { ObjectMetadataNavItemsSkeletonLoader } from '@/object-metadata/components/ObjectMetadataNavItemsSkeletonLoader';
+import {
+  ObjectMetadataNavItemsSkeletonLoader,
+} from '@/object-metadata/components/ObjectMetadataNavItemsSkeletonLoader';
 import { useFilteredObjectMetadataItems } from '@/object-metadata/hooks/useFilteredObjectMetadataItems';
 import { useIsPrefetchLoading } from '@/prefetch/hooks/useIsPrefetchLoading';
 import { usePrefetchedData } from '@/prefetch/hooks/usePrefetchedData';
 import { PrefetchKey } from '@/prefetch/types/PrefetchKey';
 import { NavigationDrawerItem } from '@/ui/navigation/navigation-drawer/components/NavigationDrawerItem';
 import { NavigationDrawerSection } from '@/ui/navigation/navigation-drawer/components/NavigationDrawerSection';
-import { NavigationDrawerSectionTitle } from '@/ui/navigation/navigation-drawer/components/NavigationDrawerSectionTitle';
+import {
+  NavigationDrawerSectionTitle,
+} from '@/ui/navigation/navigation-drawer/components/NavigationDrawerSectionTitle';
 import { useNavigationSection } from '@/ui/navigation/navigation-drawer/hooks/useNavigationSection';
 import { View } from '@/views/types/View';
 import { getObjectMetadataItemViews } from '@/views/utils/getObjectMetadataItemViews';
@@ -42,60 +46,32 @@ export const ObjectMetadataNavItems = ({ isRemote }: { isRemote: boolean }) => {
         />
 
         {isNavigationSectionOpen &&
-          [
-            ...filteredActiveObjectMetadataItems
-              .filter((item) =>
-                ['person', 'company', 'opportunity'].includes(
-                  item.nameSingular,
-                ),
-              )
-              .sort((objectMetadataItemA, objectMetadataItemB) => {
-                const order = ['person', 'company', 'opportunity'];
-                const indexA = order.indexOf(objectMetadataItemA.nameSingular);
-                const indexB = order.indexOf(objectMetadataItemB.nameSingular);
-                if (indexA === -1 || indexB === -1) {
-                  return objectMetadataItemA.nameSingular.localeCompare(
-                    objectMetadataItemB.nameSingular,
-                  );
-                }
-                return indexA - indexB;
-              }),
-            ...filteredActiveObjectMetadataItems
-              .filter(
-                (item) =>
-                  !['person', 'company', 'opportunity'].includes(
-                    item.nameSingular,
-                  ),
-              )
-              .sort((objectMetadataItemA, objectMetadataItemB) => {
-                return new Date(objectMetadataItemA.createdAt) <
-                  new Date(objectMetadataItemB.createdAt)
-                  ? 1
-                  : -1;
-              }),
-          ].map((objectMetadataItem) => {
-            const objectMetadataViews = getObjectMetadataItemViews(
-              objectMetadataItem.id,
-              views,
-            );
-            const viewId = objectMetadataViews[0]?.id;
+          filteredActiveObjectMetadataItems
+            .sort((objectMetadataItemA, objectMetadataItemB) =>
+              objectMetadataItemA.labelPlural.localeCompare(objectMetadataItemB.labelPlural))
+            .map((objectMetadataItem) => {
+              const objectMetadataViews = getObjectMetadataItemViews(
+                objectMetadataItem.id,
+                views,
+              );
+              const viewId = objectMetadataViews[0]?.id;
 
-            const navigationPath = `/objects/${objectMetadataItem.namePlural}${
-              viewId ? `?view=${viewId}` : ''
-            }`;
+              const navigationPath = `/objects/${objectMetadataItem.namePlural}${
+                viewId ? `?view=${viewId}` : ''
+              }`;
 
-            return (
-              <NavigationDrawerItem
-                key={objectMetadataItem.id}
-                label={objectMetadataItem.labelPlural}
-                to={navigationPath}
-                active={
-                  currentPath === `/objects/${objectMetadataItem.namePlural}`
-                }
-                Icon={getIcon(objectMetadataItem.icon)}
-              />
-            );
-          })}
+              return (
+                <NavigationDrawerItem
+                  key={objectMetadataItem.id}
+                  label={objectMetadataItem.labelPlural}
+                  to={navigationPath}
+                  active={
+                    currentPath === `/objects/${objectMetadataItem.namePlural}`
+                  }
+                  Icon={getIcon(objectMetadataItem.icon)}
+                />
+              );
+            })}
       </NavigationDrawerSection>
     )
   );
