031f9c8dcac7cb5e9ce546c70757096cec2ce91d
JarWarren <wrrn24@gmail.com>
2024-10-07 00:05:53 -0600

    city field

--- packages/twenty-front/src/modules/favorites/components/WorkspaceFavorites.tsx
+++ packages/twenty-front/src/modules/favorites/components/WorkspaceFavorites.tsx
@@ -1,11 +1,11 @@
 import { useFavorites } from '@/favorites/hooks/useFavorites';
-import { NavigationDrawerSectionForObjectMetadataItems } from '@/object-metadata/components/NavigationDrawerSectionForObjectMetadataItems';
 import { NavigationDrawerSectionForObjectMetadataItemsSkeletonLoader } from '@/object-metadata/components/NavigationDrawerSectionForObjectMetadataItemsSkeletonLoader';
 import { useFilteredObjectMetadataItems } from '@/object-metadata/hooks/useFilteredObjectMetadataItems';
 import { useIsPrefetchLoading } from '@/prefetch/hooks/useIsPrefetchLoading';
 import { usePrefetchedData } from '@/prefetch/hooks/usePrefetchedData';
 import { PrefetchKey } from '@/prefetch/types/PrefetchKey';
 import { View } from '@/views/types/View';
+import { FunnelminkNavigationDrawerSectionForObjectMetadataItems } from '~/funnelmink/FunnelminkNavigationDrawerSectionForObjectMetadataItems';
 
 export const WorkspaceFavorites = () => {
   const { records: views } = usePrefetchedData<View>(PrefetchKey.AllViews);
@@ -35,7 +35,7 @@ export const WorkspaceFavorites = () => {
   }
 
   return (
-    <NavigationDrawerSectionForObjectMetadataItems
+    <FunnelminkNavigationDrawerSectionForObjectMetadataItems
       sectionTitle={'Workspace Favorites'}
       objectMetadataItems={objectMetadataItemsToDisplay}
       views={views}
